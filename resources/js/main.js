33455a3da3cb6ce1d9833e14af2878be9a55c42f
Patricio Escudero <edparra@outlook.com>
2024-12-16 02:58:05 -0600

    commit inicial

---
+++ resources/js/main.js
@@ -0,0 +1,85 @@
+const ClinicaDefault ={
+    id:0,
+    nombre:"Seleccione una Clínica...",
+    urlDireccion:""
+}
+
+const Clinica1 = {
+    id : 1,
+    nombre:"Pethouse Caprove",
+    urlDireccion:"https://maps.app.goo.gl/GTi9YhjZpfxsiBhs8"
+}
+
+const Clinica2 = {
+    id:2,
+    nombre:"Veterinaria Panda",
+    urlDireccion:"https://maps.app.goo.gl/hbNtE7i7wExRxbt47"
+}
+
+const Clinica3={
+    id:3,
+    nombre:"Veterinaria ANIMED",
+    urlDireccion: "https://maps.app.goo.gl/6SDsiu4TTiezH7ov7"
+}
+
+//Servicios
+const Servicio1 ={
+    id:1,
+    nombre:"Baño de Mascota",
+    precio:19.99
+}
+
+const Servicio2 = {
+    id:2,
+    nombre:"Desparacitación",
+    precio:56.99
+}
+
+const Servicio3 = {
+    id:3,
+    nombre:"Vacunación",
+    precio:105.69
+}
+
+//Reservaciones
+const Reservacion1 ={
+    citaID:1,
+    nombreMascota:"Shadow",
+    nombreClinica:"Pethouse Caprove",
+    urlClinica:"https://maps.app.goo.gl/juX2atSGG9qgB6Tv5",
+    fecha:"01/01/2025",
+    hora:"11:30",
+    comentario:"test reservacion 1",
+    servicio1:"",
+    servicio2:"",
+    servicio3:"",
+    montoTotal:0.00
+}
+const Reservacion2 ={
+    citaID:2,
+    nombreMascota:"Magna",
+    nombreClinica:"Veterinaria Panda",
+    urlClinica:"https://maps.app.goo.gl/juX2atSGG9qgB6Tv5",
+    fecha:"18/12/2024",
+    hora:"09:00",
+    comentario:"test reservacion 2",
+    servicio1:"",
+    servicio2:"",
+    servicio3:"",
+    montoTotal:0.00
+}
+const Reservacion3 ={
+    citaID:3,
+    nombreMascota:"Totti",
+    nombreClinica:"Veterinaria ANIMED",
+    urlClinica:"https://maps.app.goo.gl/juX2atSGG9qgB6Tv5",
+    fecha:"02/03/2025",
+    hora:"15:30",
+    comentario:"test reservacion 3",
+    servicio1:"",
+    servicio2:"",
+    servicio3:"",
+    montoTotal:0.00
+}
+
+
